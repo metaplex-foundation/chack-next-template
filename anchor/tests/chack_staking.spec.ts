436040f95045ee41c0f0167a0b336bc6526ac909
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-02-09 16:03:46 -0800

    Add missing accounts to stake ix and get stake test working

--- anchor/tests/chack_staking.spec.ts
+++ anchor/tests/chack_staking.spec.ts
@@ -1,9 +1,19 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
-import { MPL_BUBBLEGUM_PROGRAM_ID } from '@metaplex-foundation/mpl-bubblegum'
+import {
+  hashMetadataCreators,
+  hashMetadataData,
+  MetadataArgsArgs,
+  MPL_BUBBLEGUM_PROGRAM_ID,
+  TokenProgramVersion,
+  TokenStandard,
+} from '@metaplex-foundation/mpl-bubblegum'
+
+import { PublicKey as UmiPublicKey } from '@metaplex-foundation/umi'
 
 import {
   Keypair,
+  LAMPORTS_PER_SOL,
   PublicKey,
   SystemProgram,
   Transaction,
@@ -11,12 +21,18 @@ import {
 
 import {
   getConcurrentMerkleTreeAccountSize,
+  ConcurrentMerkleTreeAccount,
   SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
   SPL_NOOP_PROGRAM_ID,
   ValidDepthSizePair,
 } from '@solana/spl-account-compression';
 
 import { ChackStaking } from '../target/types/chack_staking';
+
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 describe('chack_staking', () => {
   // Configure the client to use the local cluster.
@@ -31,19 +47,19 @@ describe('chack_staking', () => {
   const cNftOwnerKeypair = Keypair.generate();
   const cNftOwner = cNftOwnerKeypair.publicKey;
 
-  // Create the Merkle tree account with maxDepth and maxBufferSize.
+  // This type enforces a valid `maxDepth` and `maxBufferSize` pair.
   const depthSizePair: ValidDepthSizePair = {
     maxDepth: 3,
     maxBufferSize: 8,
   };
 
-  // Find the treeConfig PDA.
+  // Find the `treeConfig` PDA.
   const [treeConfig, _treeConfigBump] = PublicKey.findProgramAddressSync(
     [merkleTree.toBuffer()],
     new PublicKey(MPL_BUBBLEGUM_PROGRAM_ID),
   );
 
-  // Find the treeOwner PDA.
+  // Find the `treeOwner` PDA.
   const [treeOwner, _treeOwnerBump] = PublicKey.findProgramAddressSync(
     [
       anchor.utils.bytes.utf8.encode('tree_owner'),
@@ -62,8 +78,32 @@ describe('chack_staking', () => {
     program.programId
   )
 
+  // This is the same Metadata that the program uses.
+  const metadata: MetadataArgsArgs  = {
+    name: 'cNFT',
+    symbol: 'cNFT',
+    uri: 'https://c.nft',
+    creators: [{
+      address: treeOwner as unknown as UmiPublicKey,
+      verified: false,
+      share: 100,
+    }],
+    editionNonce: null,
+    tokenProgramVersion: TokenProgramVersion.Original,
+    tokenStandard: TokenStandard.NonFungible,
+    uses: null,
+    collection: null,
+    primarySaleHappened: true,
+    sellerFeeBasisPoints: 500,
+    isMutable: true,
+  };
+
+  // These are the hashes needed for transferring.
+  const dataHash = Array.from(hashMetadataData(metadata));
+  const creatorHash = Array.from(hashMetadataCreators(metadata.creators));
+
   it('Create a Tree with ChackStaking', async () => {
-    // Create Merkle Tree account.
+    // Create Merkle tree account.
     await createMerkleTreeAccount(
       provider,
       merkleTreeKeypair,
@@ -88,6 +128,7 @@ describe('chack_staking', () => {
   });
 
   it('Mint a cNFT', async () => {
+    // Mint a cNFT owned by `cNftOwnerKeypair`.
     await program.methods
       .mintCnft()
       .accounts({
@@ -105,36 +146,50 @@ describe('chack_staking', () => {
       });
   });
 
-  // it('Stake a cNFT', async () => {
-  //   // TODO Get all this stuff for real.
-  //   const root = new Array(32).fill(0);
-  //   const dataHash = new Array(32).fill(0);
-  //   const creatorHash = new Array(32).fill(0);
-  //   const nonce = 0;
-  //   const index = 0;
+  it('Stake a cNFT', async () => {
+    // The `cNftOwnerKeypair` will need some lamports since the stake instruction
+    // initializes the `stakingDetails` PDA.
+    await provider.connection.requestAirdrop(cNftOwner, LAMPORTS_PER_SOL);
 
-  //   // TODO: Add proofs.
+    // Get the current Merkle tree root from the account.
+    const accountInfo = await provider.connection.getAccountInfo(merkleTree, { commitment: 'confirmed' });
+    const account = ConcurrentMerkleTreeAccount.fromBuffer(accountInfo!.data!);
+    const root = Array.from(account.getCurrentRoot())
 
-  //   await program.methods
-  //     .stakeCnft(root, dataHash, creatorHash, nonce, index)
-  //     .accounts({
-  //       treeConfig,
-  //       merkleTree,
-  //       owner: cNftOwner,
-  //       stakingDetails,
-  //       mplBubblegumProgram: MPL_BUBBLEGUM_PROGRAM_ID,
-  //      })
-  //      .signers([cNftOwnerKeypair])
-  //      .rpc({
-  //       skipPreflight:true
-  //     });
+    // Stake `cNftOwnerKeypair`'s cNFT.
+    try {
+      await program.methods
+      .stakeCnft(
+        root,
+        dataHash,
+        creatorHash,
+        new anchor.BN(0),
+        new anchor.BN(0),
+      ).accounts({
+        treeConfig,
+        merkleTree,
+        owner: cNftOwner,
+        stakingDetails,
+        mplBubblegumProgram: MPL_BUBBLEGUM_PROGRAM_ID,
+        logWrapper: SPL_NOOP_PROGRAM_ID,
+        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+       })
+       .signers([cNftOwnerKeypair])
+       .rpc({
+        skipPreflight:true
+      });
+    } catch(err) {
+      console.log(err.message);
+      await delay(30000);
+    }
 
-  //   const currentStakingDetails = await program.account.stakingDetails.fetch(
-  //     stakingDetails
-  //   );
+    // Verify `cNftOwnerKeypair`'s `PublicKey` is stored in the `stakingDetails` account.
+    const currentStakingDetails = await program.account.stakingDetails.fetch(
+      stakingDetails
+    );
 
-  //   expect(currentStakingDetails.owner).toEqual(cNftOwner);
-  // });
+    expect(currentStakingDetails.owner).toEqual(cNftOwner);
+  }, 50000);
 
   // it('Unstake a cNFT', async () => {
   //   // TODO Get all this stuff for real.
